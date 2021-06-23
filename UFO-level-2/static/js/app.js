7adb96f6bfe86a806b5e5a47395e82c3e6217657
Gumball <jsurgeon29@gmail.com>
2021-06-23 15:01:36 -0700

    finished bonus acitivty

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -49,16 +49,52 @@ function runEnter() {
     // prevent refreshing
     d3.event.preventDefault();
 
-    // select input html element and grab the value inserted
-    var inputValue = d3.select("#datetime").property("value");
+    // select all input html elements and grab the values inserted
+    var dateValue = d3.select("#datetime").property("value");
+    var cityValue = d3.select("#city").property("value");
+    var stateValue = d3.select("#state").property("value");
+    var countryValue = d3.select("#country").property("value");
+    var shapeValue = d3.select("#shape").property("value");
+
+    //////////////////////
+    // filter the data //
+    ////////////////////
+
+    // create variable to hold all of tableData, will skim non-matching results as we progress through each filter value
+    var results = tableData;
+    // create boolean as a flag to see if there are results to filter. 
+    var empty = true;
     
-    // use user input to filter data
-    var results = tableData.filter(entry => entry.datetime === inputValue);
+    // if input values exist, filter the results by the value + set 'empty' to false
+    if (dateValue) {
+        results = results.filter(entry => entry.datetime===dateValue);
+        empty = false;
+    }
+    if (cityValue) {
+        results = results.filter(entry => entry.city === cityValue.toLowerCase());
+        empty = false;
+    }
+    if (stateValue) {
+        results = results.filter(entry => entry.state === stateValue.toLowerCase());       
+        empty = false;
+    }
+    if (countryValue) {
+        results = results.filter(entry => entry.country === countryValue.toLowerCase());
+        empty = false;
+    }
+    if (shapeValue) {
+        results = results.filter(entry => entry.shape === shapeValue.toLowerCase());
+        empty = false;
+    }
 
-    // clear the table before populating with results
-    tbody.html("");
+    // check to see if there were entries to filter. if empty, do nothing (will leave tableData alone i.e., applies no filters); if not empty, add the filtered results.
+    if(!empty) {
+        // input was made by the user //
 
-    // call addEntries to display the filtered results (i.e., add to the table via d3)
-    addEntries(results);
+        // clear the table before populating with results
+        tbody.html("");
+        // call addEntries to display the filtered results (i.e., add to the table via d3)
+        addEntries(results);
+    }
 
 }
